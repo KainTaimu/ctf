dfca2ce1b5d17a2f88a1fc3eb31e0a7090962813
KainTaimu <erwin.panergo1@gmail.com>
2025-11-27 11:00:53 -0700

    read flag from file for hash_gen

--- hash_gen.py
+++ hash_gen.py
@@ -1,10 +1,11 @@
 from blake3 import blake3  # noqa: F403
 
-FLAG = "clubeh{71M1NG_4774CK}"
+with open("./flag.txt", "r") as f:
+    flag = f.readline()
 
 hashes = []
 hasher = blake3()
-for s in FLAG:
+for s in flag:
     hasher = hasher.update(bytes(s, "utf-8"))
     hash_hex = hasher.hexdigest()
     hashes.append(hash_hex)
